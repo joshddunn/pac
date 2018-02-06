a6f85e881365113f622b2829ee30f0997cc9573f
Josh Dunn <joshdunn.uwo@gmail.com>
2018-02-05 23:00:20 -0500

    restructure PAC module

--- pac.rb
+++ pac.rb
@@ -4,12 +4,12 @@ require "./lib/class/pac.rb"
 
 case ARGV[0..1].join("_")
 when "compile"
-  PAC.new.compile
+  PAC.compile
 when "generate_mode"
-  PAC.new.generate_mode(ARGV[2].downcase)
+  PAC::Mode.new(ARGV[2].downcase).generate
   puts "#{ARGV[2]} mode created."
 when "destroy_mode"
-  PAC.new.destroy_mode(ARGV[2].downcase)
+  PAC::Mode.new(ARGV[2].downcase).destroy
   puts "#{ARGV[2]} mode destroyed."
 else
   puts "That is not a valid PAC command."
